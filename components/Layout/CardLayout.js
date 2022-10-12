563dfe25a00871a95eeca2ff7b5c95d7497d4d18
EleoRomario <eleo.romario@gmail.com>
2022-10-12 10:14:10 -0500

    alumno

--- components/Layout/CardLayout.js
+++
@@ -1,5 +0,0 @@
-export const CardLayout = ({children}) => {
-  return (
-    <div className="bg-white rounded-lg p-5 w-[40rem] h-[80vh] relative" >{children}</div>
-  )
-}
--- pages/alumno/matricula/index.js
+++ pages/alumno/matricula/index.js
@@ -1,7 +1,20 @@
-import { CardLayout } from "../../../components/Layout/CardLayout";
+import { Button } from "@material-tailwind/react";
+import { NavArrowLeft } from "iconoir-react";
+import Link from "next/link";
+import { CardLayout } from "../../../components/Layout/alumno/CardLayout";
 
 export default function Matricula() {
-  return (
-    <CardLayout></CardLayout>
-  )
+	return (
+		<CardLayout>
+    <Link href="/alumno">
+			<Button
+      as="a"
+				variant="outlined"
+				className="bg-transparent text-gray-700 font-extralight flex flex-row p-1 border border-gray-500 _focus:outline-none _focus:border-1"
+			>
+				<NavArrowLeft /> Atras
+			</Button>
+      </Link>
+		</CardLayout>
+	);
 }
