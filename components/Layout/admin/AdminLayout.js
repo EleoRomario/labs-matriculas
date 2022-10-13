bec2f348a70be1f7221579dd167f11c030ce54cd
EleoRomario <eleo.romario@gmail.com>
2022-10-13 10:34:23 -0500

    style update

--- components/Layout/admin/AdminLayout.js
+++ components/Layout/admin/AdminLayout.js
@@ -8,7 +8,7 @@ export const AdminLayout = ({ children }) => {
 
 	return (
 		<div className="bg-white/80 backdrop-blur-sm rounded-lg w-[40rem] h-[90vh] relative">
-			<div className="w-100 h-20 flex justify-between items-center p-5">
+			<div className="w-full h-20 flex justify-between items-center p-5">
 				<div className="flex flex-row justify-between items-center">
 					<User className="text-white bg-unsa-500 rounded-full p-2 h-10 w-10" />
 					<div className="flex flex-col ml-5">
@@ -32,7 +32,7 @@ export const AdminLayout = ({ children }) => {
 					</Tooltip>
 				</div>
 			</div>
-			<div className="flex h-[68vh] flex-col gap-3 items-start bg-white m-8 rounded-lg p-5 relative">
+			<div className="flex h-[72vh] flex-col gap-3 items-start bg-white m-8 rounded-lg p-5  mt-0 relative">
 				{children}
 			</div>
 		</div>
