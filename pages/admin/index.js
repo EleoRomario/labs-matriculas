31621bcffaa93d53eff8df6d0c7775e2ec51fca3
EleoRomario <eleo.romario@gmail.com>
2022-10-12 22:25:00 -0500

    matriculas

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,4 +1,4 @@
-import { Book, DashboardSpeed, Group } from "iconoir-react";
+import { Book, DashboardSpeed, Group, PasteClipboard } from "iconoir-react";
 import Link from "next/link";
 import { useAuthContext } from "../../auth/AuthContext";
 import { AdminLayout } from "../../components/Layout/admin/AdminLayout";
@@ -7,7 +7,10 @@ export default function Admin() {
 
   return (
 		<AdminLayout>
-			<h1 className="w-full bg-unsa-200 text-white p-2 rounded flex gap-2 items-center"> <DashboardSpeed /> Dashboard</h1>
+			<h1 className="w-full bg-unsa-200 text-white p-2 rounded flex gap-2 items-center">
+				{" "}
+				<DashboardSpeed /> Dashboard
+			</h1>
 			<div className="flex flex-row flex-wrap items-center justify-center gap-4 border border-gray-200 rounded w-full p-2">
 				<Link href="/admin/alumnos">
 					<a className="flex gap-2 w-52 border border-gray-300 rounded border-l-4 p-4 hover:border-unsa-100 cursor-pointer text-gray-600 hover:text-unsa-100">
@@ -19,6 +22,12 @@ export default function Admin() {
 					<a className="flex gap-2 w-52 border border-gray-300 rounded border-l-4 p-4 hover:border-unsa-100 cursor-pointer text-gray-600 hover:text-unsa-100">
 						<Book />
 						Cursos
+					</a>
+				</Link>
+				<Link href="/admin/matriculas">
+					<a className="flex gap-2 w-52 border border-light-blue-100 rounded border-l-4 p-4 hover:border-light-blue-600 cursor-pointer text-light-blue-100 hover:text-light-blue-600">
+						<PasteClipboard />
+						Matriculas
 					</a>
 				</Link>
 			</div>
---
+++ pages/admin/matriculas/[año]/index.js
@@ -0,0 +1,249 @@
+import {
+	Accordion,
+	AccordionBody,
+	AccordionHeader,
+	Breadcrumbs,
+	Button,
+	Dialog,
+	IconButton,
+	Popover,
+	PopoverContent,
+	PopoverHandler,
+	Tooltip,
+} from "@material-tailwind/react";
+import {
+	AddUser,
+	Book,
+	BookmarkBook,
+	ClockOutline,
+	EditPencil,
+	HomeSimpleDoor,
+	Trash,
+	User,
+	WarningTriangleOutline,
+} from "iconoir-react";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { Fragment, useEffect, useState } from "react";
+import toast from "react-hot-toast";
+import { AdminLayout } from "../../../../components/Layout/admin/AdminLayout";
+import { useCursos } from "../../../../hook/useCursos";
+
+export default function Año({ año, cursos }) {
+	const añoString =
+		año === "1"
+			? "Primer año"
+			: año === "2"
+			? "Segundo año"
+			: año === "3"
+			? "Tercer año"
+			: año === "4"
+			? "Cuarto año"
+			: "Quinto año";
+
+	const [open, setOpen] = useState(false);
+	const [idUser, setIdCurso] = useState(null);
+
+	const router = useRouter();
+
+	const { deleteCurso, loadingDeleteCurso } = useCursos();
+
+	const onDelete = async (id) => {
+		await deleteCurso(id);
+	};
+
+	const [openGrupo, setOpenGrupo] = useState(0);
+
+	const handleOpenGrupo = (value) => {
+		setOpenGrupo(openGrupo === value ? 0 : value);
+	};
+
+	return (
+		<AdminLayout>			
+			<Breadcrumbs fullWidth>
+				<Link href="/admin">
+					<a className="opacity-60">
+						<HomeSimpleDoor />{" "}
+					</a>
+				</Link>
+				<Link href="/admin/matriculas">
+					<a className="opacity-60">Cursos por año </a>
+				</Link>
+				<a>{añoString} </a>
+			</Breadcrumbs>
+			<div className="w-full flex flex-col gap-3 border border-gray-100 p-4 rounded overflow-y-scroll">
+				<div className="w-full flex justify-between items-center">
+					<p>Cursos del {añoString}</p>					
+				</div>
+				<div className="w-full h-[1px] bg-blue-gray-100" />
+				<div className="w-full flex flex-col gap-3">
+					{cursos.length > 0 ? (
+						cursos.map((curso) => (
+							<div
+								key={curso.id}
+								className="border border-gray-100 py-2 px-4 rounded cursor-pointer hover:bg-cyan-50 capitalize flex justify-between items-center"
+							>
+								{" "}
+								<div>
+									<Popover placement="top-start">
+										<PopoverHandler>
+											<div className="w-full">
+												<div className="text-gray-600 flex gap-2 hover:text-cyan-800">
+													<Book />
+													{curso.nombre}
+												</div>
+											</div>
+										</PopoverHandler>
+										<PopoverContent className="bg-cyan-100 flex flex-col gap-2">
+											<p className="flex gap-2 capitalize text-cyan-800">
+												<span className="font-medium">
+													Curso:
+												</span>
+												{curso.nombre}
+											</p>
+											<div className="flex flex-col gap-2 capitalize text-cyan-800 bg-cyan-200 p-2 rounded">
+												<h1 className="font-medium">
+													Grupos
+												</h1>
+												<div>
+													{curso.grupos.map(
+														(
+															{
+																horario,
+																capacidad,
+																docente,
+																nombre,
+															},
+															index
+														) => (
+															<Accordion
+																open={
+																	openGrupo ===
+																	index + 1
+																}
+																key={index}
+																className="flex gap-2 border border-cyan-100 rounded p-1 flex-col"
+															>
+																<AccordionHeader
+																	onClick={() =>
+																		handleOpenGrupo(
+																			index +
+																				1
+																		)
+																	}
+																	className="text-xs p-1"
+																>
+																	<span className="font-medium ">
+																		Grupo:{" "}
+																	</span>
+																	{nombre}
+																</AccordionHeader>
+																<AccordionBody>
+																	<div>
+																		<span className="font-medium">
+																			Docente:{" "}
+																		</span>
+																		{
+																			docente
+																		}
+																	</div>
+																	<div>
+																		<span className="font-medium">
+																			Capacidad:{" "}
+																		</span>
+																		{
+																			capacidad
+																		}
+																	</div>
+																	<div>
+																		<span className="font-medium">
+																			Dia:{" "}
+																		</span>
+																		{
+																			horario.dia
+																		}
+																	</div>
+																	<div className="flex gap-2">
+																		<span className="font-medium">
+																			<ClockOutline />
+																		</span>
+																		{
+																			horario.horaInicio
+																		}{" "}
+																		-{" "}
+																		{
+																			horario.horaFin
+																		}
+																	</div>
+																</AccordionBody>
+															</Accordion>
+														)
+													)}
+												</div>
+											</div>
+										</PopoverContent>
+									</Popover>
+								</div>
+								<div className="flex gap-2">
+									<Tooltip
+										content="Editar curso"
+										placement="top"
+										className="bg-light-blue-200"
+									>
+										<div>
+											<Link
+												href={`/admin/cursos/${año}/${curso.id}/editar`}
+											>
+												<IconButton
+													className="bg-white border shadow-none flex p-1 gap-1 font-thin items-center border-light-blue-100 text-light-blue-100 hover:bg-light-blue-400 hover:border-light-blue-400 hover:text-white focus:outline-none active:border-none"
+													as="a"
+												>
+													<EditPencil />
+												</IconButton>
+											</Link>
+										</div>
+									</Tooltip>
+									<Tooltip
+										content="Eliminar curso"
+										placement="top"
+										className="bg-red-200"
+									>
+										<IconButton
+											variant="outlined"
+											className="flex p-1 gap-1 font-thin items-center border-red-100 text-red-100 hover:bg-red-400 hover:border-red-400 hover:text-white"
+											onClick={() => {
+												setOpen(!open);
+												setIdCurso(curso.id);
+											}}
+										>
+											<Trash />
+										</IconButton>
+									</Tooltip>
+								</div>
+							</div>
+						))
+					) : (
+						<div className="text-center text-gray-500">
+							<p>No hay cursos registrados en este año</p>
+						</div>
+					)}
+				</div>
+			</div>
+		</AdminLayout>
+	);
+}
+
+export const getServerSideProps = async (context) => {
+	const { params } = context;
+	const { año } = params;
+
+	const res = await fetch(`http://localhost:3000/api/cursos/${año}`);
+	const cursos = await res.json();
+
+	return {
+		props: {
+			año,
+			cursos,
+		},
+	};
+};
